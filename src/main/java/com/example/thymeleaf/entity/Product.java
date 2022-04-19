1d4783216098fa87b95ede52bf95f5bb8856c953
Anton Loshmanov <pppkms2339@gmail.com>
2022-04-19 11:16:35 +1000

    1. Создали шаблоны index и product. 2. Создали файлы сущностей Product и ProductType

---
+++ src/main/java/com/example/thymeleaf/entity/Product.java
@@ -0,0 +1,39 @@
+package com.example.thymeleaf.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+
+@Data
+@Entity(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Column(name = "new_product")
+    private boolean newProduct;
+
+    @Column(name = "hot_product")
+    private boolean hotProduct;
+
+    private double price;
+
+    @Column(name = "old_price")
+    private double oldPrice;
+
+    private String image;
+
+    @Column(length = 65535)
+    @Type(type = "text")
+    private String description;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="product_type_id")
+    private ProductType productType;
+
+}
--- src/main/java/com/example/todo/repository/ToDoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todo.repository;
-
-import com.example.todo.domain.ToDo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ToDoRepository extends CrudRepository<ToDo, String> {
-}
